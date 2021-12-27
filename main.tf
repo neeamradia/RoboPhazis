96571c0d4d743034869b12ead426bfb64aaaf25e
neeamradia <neeam.radia@gmail.com>
2021-12-27 02:17:02 +0000

    Updated Terraform Cloud organisation/workspace.

--- main.tf
+++ main.tf
@@ -15,6 +15,11 @@
 }*/
 
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
   backend "remote" {
     hostname = "app.terraform.io"
     organization = "neeamradia"
